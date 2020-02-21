51d3f2919fd4c3bbf639c968f2834de93b3252b6
Feng Liying <sereneliying@outlook.com>
2020-02-21 11:45:41 +0800

    demo1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+              './app2.component.css']
 })
 export class AppComponent {
   title = 'dipsaday1';
+  message=0;
+  isDisabled=true;
+  message2=0;
+
+  blink(){
+    console.log("Hello World !");
+    this.message= this.message+1;
+    this.isDisabled=false;
+  }
+
+  slashYou(){
+    console.log("Slash !");
+    this.message2=this.message+2;
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CartComponent } from './components/cart.component';
+import { PaymentComponent } from './components/payment.component';
+import { ShippingComponent } from './components/shipping/shipping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    PaymentComponent,
+    ShippingComponent
   ],
   imports: [
     BrowserModule
